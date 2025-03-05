f02fc736ae43c6b6302e05945620c7b54352f8bf
KOH Virgil Shaun <virgil-shaun.koh@etu.iut-tlse3.fr>
2025-03-05 14:41:45 +0100

    Copying vues to repo

---
+++ matchs.php
@@ -0,0 +1,60 @@
+<?php include_once "../components/nav.php" ?>
+
+<div class="main div-column">
+    <header class="header-section">
+        <h1>Liste des matchs du <?= $_SESSION["equipe"]?></h1>
+        <div class="add">
+            <p class="color-red">Vous cherchez à ajouter un match?</p>
+            <a href="../controleur/gerermatch.php?type=ajout&idMatch=0&csrf_token=<?= htmlspecialchars(password_hash("0" . $_SESSION['csrf_token'] . "ajout", PASSWORD_BCRYPT)) ?>" class="forms button add"><p>Ajouter un match</p></a>
+        </div>
+    </header>
+    <article class="match-list">
+        <?php
+        if(empty($matchs)){
+            echo "<p class=\"color-red\">Aucun match n'est enregistré pour le moment.</p>";
+        }
+        foreach ($matchs as $match) { ?>
+
+            <section class="section-card">
+
+                <div class="card-info">
+                    <div class="head">
+                        <h2>Match du <?= $match->getDateHeure()->format('d/m/Y-H:i') ?></h2>
+                        <p><strong>Adversaire:</strong> <?= $match->getAdversaire() ?></p>
+                        <p><strong>Lieu:</strong> <?= $match->getLieu()->name ?></p>
+                    </div>
+                </div>
+
+                <div class="statut">
+                    <?php if ($match->getDateHeure() < new DateTime()) { ?>
+                        <p class="result">Match passé</p>
+                        <?php if(!is_null($match->getResultat())){ ?>
+                            <p class="result">Match validé</p>
+                            <p>Score : <?= $match->getResultat()->name?></p>
+                        <?php }
+                        else { ?>
+                            <p>Aucun résultat</p>
+                        <?php }
+                    } else { ?>
+                        <p class="result">Match à venir</p>
+                    <?php } ?>
+                </div>
+                <div class="forms">
+
+                    <a href="../controleur/gerermatch.php?type=vue&idMatch=<?= htmlspecialchars($match->getIdMatch()) ?>&csrf_token=<?= htmlspecialchars(password_hash($match->getIdMatch() . $_SESSION['csrf_token'] . "vue", PASSWORD_BCRYPT)) ?>" class="forms saisie button"><p>Voir la feuille de match</p></a>
+                    <?php if($match->isValider() || JouerUnMatch::isArchiveFDM($match->getIdMatch())) {
+                        echo "<p class=\"color-red\" style='text-align: center'>Ce match est archivé et ne peut être modifié.</p>";
+                    } else {?>
+                        <a href="../controleur/gerermatch.php?type=modification&idMatch=<?= htmlspecialchars($match->getIdMatch()) ?>&csrf_token=<?= htmlspecialchars(password_hash($match->getIdMatch() . $_SESSION['csrf_token'] . "modification", PASSWORD_BCRYPT)) ?>" class="forms modify button"><p>Modifier le match</p></a>
+                    <?php } ?>
+                    <form method="post" action="../controleur/gerermatch.php">
+                        <input type="hidden" name="type" value="suppression">
+                        <input type="hidden" name="idMatch" value="<?= htmlspecialchars($match->getIdMatch()) ?>">
+                        <input type="hidden" name="csrf_token" value="<?= htmlspecialchars(password_hash($match->getIdMatch() . $_SESSION['csrf_token'] . "suppression", PASSWORD_BCRYPT)) ?>">
+                        <button type="submit" class="delete">Supprimer</button>
+                    </form>
+                </div>
+            </section>
+        <?php } ?>
+    </article>
+</div>
---
+++ vuematch.php
@@ -0,0 +1,107 @@
+<?php include_once "../components/nav.php";
+function createPlayerCard($joueur): string
+{
+    if($joueur->getUrl() == ""){
+        $url = "../resources/img/data/default.png";
+    }else{
+        $url = $joueur->getUrl();
+    }
+    $class = $joueur->isPremiereLigne() ? 'premiere-ligne': '';
+    return '<div class="player-card '.$class.'" data-id="1">
+        <img src="' .$url.'" alt="Profile picture of '.$joueur->getNom() . " " . $joueur->getPrenom().'" width="40" height="40">
+        <p>'.$joueur->getNom() . ' ' . $joueur->getPrenom().'</p>
+        <input type="hidden" name="id" value="'.$joueur->getIdJoueur().'">
+        <input type="hidden" name="premiereLigne" value="'.$joueur->isPremiereLigne().'">
+    </div>';
+}
+?>
+<div class="main">
+    <header class="header-section">
+        <h1>Faites votre feuille de match</h1>
+    </header>
+    <article>
+        <div class="container">
+            <aside>
+                <p id="">Il y a <strong id="fieldNbJoueurs"></strong> joueurs sur la feuille de match</p>
+                <p id="">Il y a <strong id="fieldNbPremieresLignes"></strong> joueurs ayant la spécificité 1ère ligne sur la feuille de match</p>
+
+                <?php if($valider || $archive){?>
+                    <p>Le match est archivé, vous ne pouvez plus le modifier</p>
+                <?php } else {?>
+
+                    <div class="players-container" id="players">
+                        <!-- Example Player Cards -->
+
+                        <?php
+                        if($joueursNP) {
+                            foreach ($joueursNP as $joueurNP) {
+                                echo createPlayerCard($joueurNP);
+                            }
+                        }else {
+                            echo "<p>Vous n'avez aucun joueur actif</p>";
+                        }?>
+                    </div>
+                <?php } ?>
+                <div>
+            </aside>
+            <form action="gererfdm.php" method="post" class="form">
+                <div class="field" id="field">
+                    <!-- Position slots -->
+                    <?php
+                    $nbJoueurs = 0;
+                    $nbPremieresLignes = 0;
+                    for($i = 1; $i <= 23; $i++){
+                        $value = ""?>
+                        <div class="position-slot slot-<?=$i?>" data-position="<?=$i?>"><?php if(array_key_exists($i,$jouerLeMatch)) {
+                                $nbJoueurs++;
+                                if($jouerLeMatch[$i]->getJoueur()->isPremiereLigne()){
+                                    $nbPremieresLignes++;
+                                }
+                                echo createPlayerCard($jouerLeMatch[$i]->getJoueur());
+                                $value = $jouerLeMatch[$i]->getJoueur()->getIdJoueur();
+                            } ?></div>
+                        <input type="hidden" name="position-<?=$i?>" value="<?= $value?>">
+                    <?php } ?>
+                </div>
+                <input name="nbJoueurs" type="hidden" value="" id="inputNbJoueurs">
+                <input name="nbPremieresLignes" type="hidden" value="" id="inputNbPremieresLignes">
+                <input type="hidden" name="type" value="ajout">
+                <input type="hidden" name="csrf_token" value="<?= htmlspecialchars(password_hash($_GET["idMatch"] . $_SESSION['csrf_token'] . "ajout", PASSWORD_BCRYPT)) ?>">
+                <input type="hidden" name="idMatch" value="<?=$_GET["idMatch"]?>">
+                <?php if($archive){ ?>
+                    <p>Le match est archivé, vous ne pouvez plus le modifier</p>
+                    <p>Résultat : <?=$match->getResultat()->name?></p>
+                    <a class="button saisie" href="/gererfdm.php?type=notes&idMatch=<?=$match->getIdMatch()?>&csrf_token=<?=htmlspecialchars(password_hash($match->getIdMatch().$_SESSION["csrf_token"]."notes",PASSWORD_BCRYPT))?>">
+                        <p>Saisir les notes</p>
+                    </a>
+                <?php } elseif($valider){ ?>
+
+                    <div class="row">
+                        <p>Feuille de match validée, vous pouvez dès à présent finaliser le match en saisissant le score</p>
+                        <label for="resultat">Score</label>
+                        <select id="resultat" name="resultat" required>
+                            <?php foreach (Resultat::cases() as $resultat) { ?>
+                                <option value="<?= $resultat->name ?>">
+                                    <?= htmlspecialchars($resultat->name) ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <input type="hidden" name="fdm" value="0">
+                    <input type="submit" name="submit" class="button saisie" value="Saisir le score" id="btnScore">
+                <?php } else {?>
+                    <input type="hidden" name="idMatch" value="<?=$match->getIdMatch()?>">
+                    <input type="hidden" name="fdm" value="1">
+                    <input type="submit" name="submit" class="button saisie" value="ajouter">
+                    <input type="submit" name="submit" class="button modify" value="valider" id="buttonValider">
+                <?php } ?>
+            </form>
+        </div>
+    </article>
+</div>
+<script>
+    let nbJoueurs = <?php echo $nbJoueurs ?>;
+    let nbPremieresLignes = <?php echo $nbPremieresLignes ?>;
+    let archiveMatch = <?php echo $archive || $valider ? 1 : 0?>;
+</script>
+<script src="../resources/js/fdm.js"></script>
