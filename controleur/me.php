eef8a20e5ad3054367de4d43dc157ba4ba2989e7
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-18 23:04:46 +0100

    restructuration du projet

--- controleur/me.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../modele/Session.php";
-checkSession();
-
-$title = "Mon compte";
-$css = ["style.css"];
-$page = "../vue/me.php";
-include_once "../components/page.php";
--- controleur/results.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../modele/Session.php";
-checkSession();
-
-$title = "Resultats & Statistiques";
-$css = ["style.css"];
-$page = "../vue/results.php";
-include_once "../components/page.php";
