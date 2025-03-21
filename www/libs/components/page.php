921b81f073b0133fbb20e26a40009126e40339d6
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-21 17:39:57 +0100

    restructuration du composant principal pour inclure les cdn jquery et Cookies ainsi que la modification des balises de style et de l'inclusion de la navbar

--- www/libs/components/page.php
+++ www/libs/components/page.php
@@ -3,13 +3,20 @@
 <head>
     <title><?=$title?></title>
     <meta charset="utf-8"/>
-    <?php foreach ($css as $style) { ?>
-        <link rel="stylesheet" href="/resources/style/.$style" ?>">
-    <?php } ?>
     <link rel="stylesheet" href="/resources/style/main.css">
     <link rel="stylesheet" href="/resources/style/nav.css">
+    <?php foreach ($css as $style) { ?>
+        <link rel="stylesheet" href="/resources/style/<?=$style?>">
+    <?php } ?>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js"></script>
 </head>
 <body>
-<?php include_once $page; ?>
+<?php
+if($title !=="Login"){
+    include_once $_SERVER["DOCUMENT_ROOT"] . "../libs/components/nav.php";
+}
+include_once $page;
+?>
 </body>
 </html>
