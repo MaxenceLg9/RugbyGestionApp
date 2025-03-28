de2e83440070fc53e802db4d14124ee285f2292c
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-28 20:35:18 +0100

    classe pour récupérer les enums depuis l'api

---
+++ www/libs/modele/Enum.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Enum {
+
+    function getPostes():array{
+        return [];
+    }
+
+    function getResultats():array{
+        return [];
+    }
+
+    function getStatuts():array{
+        return [];
+    }
+
+    function getLieux():array{
+        return [];
+    }
+}
