2931a944cc3fdfdb280515075f39832f1204115c
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-28 20:37:09 +0100

    composant représentant l'implémentation à faire dans le js

--- www/libs/components/matchs-card.php
+++ www/libs/components/matchs-card.php
@@ -8,9 +8,9 @@
 <!---->
 <!--                <div class="card-info">-->
 <!--                    <div class="head">-->
-<!--                        <h2>Match du --><?php //= $match->getDateHeure()->format('d/m/Y-H:i') ?><!--</h2>-->
-<!--                        <p><strong>Adversaire:</strong> --><?php //= $match->getAdversaire() ?><!--</p>-->
-<!--                        <p><strong>Lieu:</strong> --><?php //= $match->getLieu()->name ?><!--</p>-->
+<!--                        <h2>Match du -->{dateHeure}<!--</h2>-->
+<!--                        <p><strong>Adversaire:</strong> -->{adversaire}<!--</p>-->
+<!--                        <p><strong>Lieu:</strong> -->{lieu}<!--</p>-->
 <!--                    </div>-->
 <!--                </div>-->
 <!---->
@@ -19,7 +19,7 @@
 <!--                        <p class="result">Match passé</p>-->
 <!--                        --><?php //if(!is_null($match->getResultat())){ ?>
 <!--                            <p class="result">Match validé</p>-->
-<!--                            <p>Score : --><?php //= $match->getResultat()->name?><!--</p>-->
+<!--                            <p>Score : -->{resultat}<!--</p>-->
 <!--                        --><?php //}
 //                        else { ?>
 <!--                            <p>Aucun résultat</p>-->
@@ -30,18 +30,13 @@
 <!--                </div>-->
 <!--                <div class="forms">-->
 <!---->
-<!--                    <a href="/gerermatch.php?type=vue&idMatch=--><?php //= htmlspecialchars($match->getIdMatch()) ?><!--&csrf_token=--><?php //= htmlspecialchars(hash_hmac("sha256", $match->getIdMatch() . $_SESSION['csrf_token'] . "vue", $_SESSION['csrf_token'])) ?><!--" class="forms saisie button"><p>Voir la feuille de match</p></a>-->
+<!--                    <a href="/gerermatch.php?type=vue&idMatch=-->{idMatch}<!--" class="forms saisie button"><p>Voir la feuille de match</p></a>-->
 <!--                    --><?php //if($match->isValider() || JouerUnMatch::isArchiveFDM($match->getIdMatch())) {
 //                        echo "<p class=\"color-red\" style='text-align: center'>Ce match est archivé et ne peut être modifié.</p>";
 //                    } else {?>
-<!--                        <a href="/gerermatch.php?type=modification&idMatch=--><?php //= htmlspecialchars($match->getIdMatch()) ?><!--&csrf_token=--><?php //= htmlspecialchars(hash_hmac("sha256", $match->getIdMatch() . $_SESSION['csrf_token'] . "modification", $_SESSION['csrf_token'])) ?><!--" class="forms modify button"><p>Modifier le match</p></a>-->
+<!--                        <a href="/gerermatch.php?type=modification&idMatch=-->{idMatch}<!--" class="forms modify button"><p>Modifier le match</p></a>-->
 <!--                    --><?php //} ?>
-<!--                    <form method="post" action="gerermatch.php">-->
-<!--                        <input type="hidden" name="type" value="suppression">-->
-<!--                        <input type="hidden" name="idMatch" value="--><?php //= htmlspecialchars($match->getIdMatch()) ?><!--">-->
-<!--                        <input type="hidden" name="csrf_token" value="--><?php //= htmlspecialchars(hash_hmac("sha256", $match->getIdMatch() . $_SESSION['csrf_token'] . "suppression", $_SESSION['csrf_token'])) ?><!--">-->
-<!--                        <button type="submit" class="delete">Supprimer</button>-->
-<!--                    </form>-->
+                    <button type="submit" class="delete" onclick="deleteMatch({idMatch})">Supprimer</button>
 <!--                </div>-->
 <!--            </section>-->
 <!--        --><?php //} ?>
