8e09d1b37b82ba6672f24319afc1593f0dac6b55
Lagourgue Maxence <maxence.lagourgue@free.fr>
2025-03-30 17:25:40 +0200

    creation du fichier FDM.php pour gérer l'endpoint GET statique /fdm

---
+++ www/libs/modele/FDM.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace FDM {
+
+    require $_SERVER["DOCUMENT_ROOT"]."/../vendor/autoload.php";
+    use GuzzleHttp\Client;
+
+    function getFDMPourJoueur(string $idJoueur): array {
+
+        $client = new Client([
+            'base_uri' => 'http://rugbygestion.api',
+            'timeout'  => 2.0,
+        ]);
+
+        $response = $client->get('/fdm.php',[
+            'query' => [
+                'idJoueur' => $idJoueur,
+            ],
+            'headers' => [
+                'Authorization' => $_COOKIE["token"] ?? "",
+                'Accept' => 'application/json',
+            ]
+        ]);
+        $feuilles = json_decode($response->getBody(),true)["data"];
+        return empty($feuilles) ? [] :$feuilles["matchs"];
+    }
+}
