9804ea9c2831d513c77c39d98c9c1482bb781343
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-31 12:53:43 +0200

    début du portage des pages de gestion des matchs

--- www/libs/vue/modifiermatch.php
+++ www/libs/vue/modifiermatch.php
@@ -3,38 +3,36 @@
 require $_SERVER["DOCUMENT_ROOT"]."../libs/modele/Enum.php" ?>
 
 <div class="main div-column">
+    <h1>Modifier un match</h1>
     <article class="article-list">
-        <h1>Modifier un match</h1>
+        <h3>Entrez les informations du match à modifier</h3>
         <section class="full">
             <h3>Entrez les informations du match à modifier</h3>
             <h3>Match du <?= $match["dateHeure"] ?> contre <?= $match["adversaire"] ?>, Lieu : <?= $match["lieu"] ?></h3>
-            <form action="gerermatch.php" method="post">
-                <table class="form-table">
-                    <tr>
-                        <td><label for="datetime">Date du match</label></td>
-                        <td class="input"><input type="datetime-local" id="datetime" name="datetime" required value="<?= $match["dateHeure"] ?>"></td>
-                    </tr>
-                    <tr>
-                        <td><label for="lieu">Lieu du match</label></td>
-                        <td class="input">
-                            <select id="lieu" name="lieu" required>
-                                <?php foreach (Enum\getLieux() as $key=>$lieuPossible) { ?>
-                                    <option value="<?= $key?>" <?= $match["lieu"] === $lieuPossible ? 'selected' : '' ?>>
-                                        <?= htmlspecialchars($lieuPossible) ?>
-                                    </option>
-                                <?php } ?>
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label for="adversaire">Adversaire</label></td>
-                        <td class="input"><input type="text" id="adversaire" name="adversaire" placeholder="Nom de l'adversaire" required value="<?= $match["adversaire"] ?>"></td>
-                    </tr>
-                </table>
-                <input type="hidden" name="idMatch" value="<?= $match["idMatch"] ?>">
-                <input type="hidden" name="type" value="modification">
-                <button type="submit" class="add">Modifier le match</button>
-            </form>
+            <table class="form-table">
+                <tr>
+                    <td><label for="datetime">Date du match</label></td>
+                    <td class="input"><input type="datetime-local" id="datetime" name="datetime" required value="<?= $match["dateHeure"] ?>"></td>
+                </tr>
+                <tr>
+                    <td><label for="lieu">Lieu du match</label></td>
+                    <td class="input">
+                        <select id="lieu" name="lieu" required>
+                            <?php foreach (Enum\getLieux() as $key=>$lieuPossible) { ?>
+                                <option value="<?= $key?>" <?= $match["lieu"] === $lieuPossible ? 'selected' : '' ?>>
+                                    <?= htmlspecialchars($lieuPossible) ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="adversaire">Adversaire</label></td>
+                    <td class="input"><input type="text" id="adversaire" name="adversaire" placeholder="Nom de l'adversaire" required value="<?= $match["adversaire"] ?>"></td>
+                </tr>
+            </table>
+            <input type="hidden" name="idMatch" value="<?= $match["idMatch"] ?>">
+            <button type="submit" class="add">Modifier le match</button>
         </section>
     </article>
 </div>
--- www/libs/vue/nouveaumatch.php
+++ www/libs/vue/nouveaumatch.php
@@ -5,32 +5,33 @@ require $_SERVER["DOCUMENT_ROOT"]."../libs/modele/Enum.php";
 use function Enum\getLieux; ?>
 
 <div class="main div-column">
+    <h1>Ajouter un match</h1>
     <article class="article-list">
-        <h1>Ajouter un match</h1>
+        <h3>Entrez les informations du match à ajouter</h3>
         <section class="full">
             <h3>Entrez les informations du match à ajouter</h3>
-            <form action="gerermatch.php" method="post">
-                <div class="form-row">
-                    <label for="datetime">Date du match</label>
-                    <input type="datetime-local" id="datetime" name="datetime" required>
-                </div>
-                <div class="form-row">
-                    <label for="lieu">Lieu du match</label>
-                    <select id="lieu" name="lieu" required>
-                        <?php foreach (getLieux() as $key=>$lieu) { ?>
-                            <option value="<?= $key ?>"><?= htmlspecialchars($lieu) ?></option>
-                        <?php } ?>
-                    </select>
-                </div>
-                <div class="form-row">
-                    <label for="adversaire">Adversaire</label>
-                    <input type="text" id="adversaire" name="adversaire" placeholder="Nom de l'adversaire" required>
-                </div>
-
-                <input type="hidden" name="idMatch" value="<?= 0 ?>">
-                <input type="hidden" name="type" value="ajout">
-                <button type="submit" class="add">Ajouter le match</button>
-            </form>
+            <table class="form-table">
+                <tr>
+                    <td><label for="datetime">Date du match</label></td>
+                    <td class="input"><input type="datetime-local" id="datetime" name="datetime" required></td>
+                </tr>
+                <tr>
+                    <td><label for="lieu">Lieu du match</label></td>
+                    <td class="input">
+                        <select id="lieu" name="lieu" required>
+                            <?php foreach (getLieux() as $key=>$lieu) { ?>
+                                <option value="<?= $key ?>"><?= htmlspecialchars($lieu) ?></option>
+                            <?php } ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="adversaire">Adversaire</label></td>
+                    <td class="input"><input type="text" id="adversaire" name="adversaire" placeholder="Nom de l'adversaire" required></td>
+                </tr>
+            </table>
+            <input type="hidden" name="idMatch" value="<?= 0 ?>">
+            <button type="submit" class="add">Ajouter le match</button>
         </section>
     </article>
 </div>
